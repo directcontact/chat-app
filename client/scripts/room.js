8fe4c8cf573a7d43acd2ed591942a2111488b33a
kimjeff49 <jeffkkim612@gmail.com>
2024-10-28 17:34:45 -0400

    adding in more modal work

--- client/scripts/room.js
+++ client/scripts/room.js
@@ -1,20 +1,32 @@
 const connecting = false;
 
-(async function () {
-  const ws = await connectToWss();
-  ws.send("Test");
+// setup for page
+(function () {
+  const username = localStorage.getItem("username");
 
-  ws.onerror = (e) => {
-    console.log(e);
-  };
-
-  ws.onopen = () => {
-    console.log("ws opened on browser");
-  };
-
-  ws.onmessage = (message) => {
-    console.log(`message received`, message.data);
-  };
+  if (username) {
+    document.querySelector(".connect-modal").remove();
+    document.querySelector(".background-filter").remove();
+    const ws = new WebSocket("ws://localhost:3001", "http");
+    ws.onopen = () => {
+      console.log("ws opened on browser");
+    };
+    ws.onmessage = (message) => {
+      console.log(`message received`, message.data);
+    };
+  }
+})(async function () {
+  // const ws = await connectToWss();
+  // ws.send("Test");
+  // ws.onerror = (e) => {
+  //   console.log(e);
+  // };
+  // ws.onopen = () => {
+  //   console.log("ws opened on browser");
+  // };
+  // ws.onmessage = (message) => {
+  //   console.log(`message received`, message.data);
+  // };
 });
 
 async function connectToWss() {
@@ -30,4 +42,20 @@ async function connectToWss() {
       }
     }, 10);
   });
+}
+
+function handleConnect() {
+  const username = document.querySelector(".connect-modal__name");
+  if (username.value) {
+    const error = document.querySelector(".connect-modal__error");
+    error.classList.remove("connect-modal__error-active");
+    error.classList.add("connect-modal__error-inactive");
+    localStorage.setItem("username", username.value);
+    document.querySelector(".connect-modal").remove();
+    document.querySelector(".background-filter").remove();
+  } else {
+    const error = document.querySelector(".connect-modal__error");
+    error.classList.add("connect-modal__error-active");
+    error.classList.remove("connect-modal__error-inactive");
+  }
 }
