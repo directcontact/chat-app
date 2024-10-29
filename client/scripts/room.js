2382f437852894c565c61f35f298b3898469101e
kimjeff49 <jeffkkim612@gmail.com>
2024-10-29 10:47:28 -0400

    adding in more robust features

--- client/scripts/room.js
+++ client/scripts/room.js
@@ -1,32 +1,36 @@
-const connecting = false;
+import svg from "./icons/user.js";
+
 let ws = null;
-let user = null;
+let self = null;
 
 const CONNECTION_EVENT = 1;
 const MESSAGE_EVENT = 2;
 const INFO_EVENT = 3;
 
+// TODO: set up more robust unique id system per user
+
 // setup for page
-(function () {
+function setup() {
   const username = localStorage.getItem("username");
 
   if (username) {
-    user = username;
+    self = username;
     document.querySelector(".connect-modal").remove();
     document.querySelector(".background-filter").remove();
     ws = new WebSocket("ws://localhost:3001", "http");
 
     // First, lets add ourselves to the list of people in the chatroom:
-    addUser(user);
+    addUser(self);
 
     ws.addEventListener("message", (event) => {
       // Handle different types of events
       // Connection events
       const data = JSON.parse(event.data);
-      console.log(data);
       if (data.type === CONNECTION_EVENT) {
         const { username: newUser, connect } = data;
-        const userContainer = document.querySelector(".room__container-users");
+        const userContainer = document.querySelector(
+          ".room__container-users--row"
+        );
 
         if (connect) {
           addUser(newUser);
@@ -36,7 +40,16 @@ const INFO_EVENT = 3;
         }
       } else if (data.type === MESSAGE_EVENT) {
         const { username, text } = data;
-        addMessage(username, text);
+        if (data.typing !== undefined) {
+          const typingUserDiv = document.getElementById(username);
+          if (data.typing) {
+            typingUserDiv.classList.add("typing");
+          } else {
+            typingUserDiv.classList.remove("typing");
+          }
+        } else {
+          addMessage(username, text);
+        }
       } else if (data.type === INFO_EVENT) {
         const { usernames } = data;
 
@@ -51,7 +64,7 @@ const INFO_EVENT = 3;
       // Send a message saying that we have connected.
       ws.send(
         JSON.stringify({
-          username: user,
+          username: self,
           connect: true,
           type: CONNECTION_EVENT,
         })
@@ -64,27 +77,58 @@ const INFO_EVENT = 3;
       textarea.removeAttribute("disabled");
       textarea.addEventListener("keydown", (e) => {
         if (e.key === "Enter") {
-          // TODO: don't let users send empty messages
+          e.preventDefault();
+          if (e.target.value) {
+            ws.send(
+              JSON.stringify({
+                username: self,
+                text: e.target.value,
+                type: MESSAGE_EVENT,
+              })
+            );
+            addMessage(self, e.target.value);
+            e.target.value = "";
+          }
+        }
+      });
+
+      textarea.addEventListener("keyup", (e) => {
+        if (!e.target.value) {
           ws.send(
             JSON.stringify({
-              username: user,
-              text: e.target.value,
+              username: self,
+              text: "",
               type: MESSAGE_EVENT,
+              typing: false,
             })
           );
-          addMessage(user, e.target.value);
+        } else {
+          ws.send(
+            JSON.stringify({
+              username: self,
+              text: "",
+              type: MESSAGE_EVENT,
+              typing: true,
+            })
+          );
         }
       });
     });
   }
-})();
+}
 
 //** HELPER FUNCTIONS */
 function addUser(username) {
-  const userContainer = document.querySelector(".room__container-users");
+  const userContainer = document.querySelector(".room__container-users--row");
   const newUserDiv = document.createElement("div");
-  newUserDiv.classList.add("room__container-users--user");
+  // If self, use different class to differentiate.
+  if (username === self) {
+    newUserDiv.classList.add("room__container-users--row---entry----self");
+  } else {
+    newUserDiv.classList.add("room__container-users--row---entry");
+  }
   newUserDiv.setAttribute("id", username);
+  newUserDiv.innerHTML = svg;
   newUserDiv.appendChild(document.createTextNode(username));
   userContainer.appendChild(newUserDiv);
 }
@@ -92,26 +136,17 @@ function addUser(username) {
 function addMessage(username, message) {
   const messageContainer = document.querySelector(".room__container-chatarea");
   const newMessageDiv = document.createElement("div");
-  newMessageDiv.classList.add("room__container-chat--message");
+  // If self, use different class to differentiate.
+  if (username === self) {
+    newMessageDiv.classList.add("room__container-chat--message---self");
+  } else {
+    newMessageDiv.classList.add("room__container-chat--message");
+  }
   newMessageDiv.appendChild(document.createTextNode(`${username}: ${message}`));
   messageContainer.appendChild(newMessageDiv);
 }
 
 //** DOM EVENT FUNCTIONS */
-
-function handleSend() {
-  if (ws.readyState) {
-    ws.send(
-      JSON.stringify({
-        username: user,
-        text: textarea.target.value,
-        type: MESSAGE_EVENT,
-      })
-    );
-  }
-  const textarea = document.querySelector(".room__container-chat--input__area");
-  addMessage(user, textarea.target.value);
-}
 
 function handleConnect() {
   const username = document.querySelector(".connect-modal__name");
@@ -120,13 +155,13 @@ function handleConnect() {
     error.classList.remove("connect-modal__error-active");
     error.classList.add("connect-modal__error-inactive");
     localStorage.setItem("username", username.value);
-    document.querySelector(".connect-modal").remove();
-    document.querySelector(".background-filter").remove();
 
-    // TODO: When a user finishes setup, add their username and get all the current users
+    setup();
   } else {
     const error = document.querySelector(".connect-modal__error");
     error.classList.add("connect-modal__error-active");
     error.classList.remove("connect-modal__error-inactive");
   }
 }
+
+setup();
